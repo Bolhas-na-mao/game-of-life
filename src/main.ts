4fa8d3780f460fe321fe819ae863ee2d3528fe42
Bruno Aseff <brunoaseff2@gmail.com>
2025-09-01 23:21:48 -0300

    feat: add simple rect

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,18 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Graphics } from "pixi.js";
 
 (async () => {
-  // Create a new application
   const app = new Application();
 
-  // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ background: "#0A0A0A", resizeTo: window });
 
-  // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  const graphics = new Graphics();
+  graphics.rect(10, 10, 50, 50).stroke({ width: 2, color: 0xffffff });
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  app.stage.addChild(graphics);
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+  graphics.position.set(0, 0);
 
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
+  app.stage.addChild(graphics);
 })();
