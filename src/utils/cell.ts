23448efbd85f01e086dd2143ac1063dc642de3f1
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-09-02 13:31:48 +0000

    refactor: replace recursive implementation with for loops

--- src/utils/cell.ts
+++ src/utils/cell.ts
@@ -1,44 +1,29 @@
 import { Application, Graphics } from "pixi.js";
-
 function renderCell(
   app: Application,
   squareArea: number,
-  amountOfRect: number,
-  verticalShift: number
+  verticalShift: number,
+  index: number
 ) {
-  if (amountOfRect <= 0) return;
-
   const graphics = new Graphics();
-
-  const horizontalShift = (amountOfRect - 1) * squareArea;
+  const horizontalShift = index * squareArea;
   graphics
     .rect(horizontalShift, verticalShift, squareArea, squareArea)
     .stroke({ width: 1, color: 0x3b3a3a });
-
   app.stage.addChild(graphics);
-
   graphics.position.set(0, 0);
-
   app.stage.addChild(graphics);
-
-  renderCell(app, squareArea, amountOfRect - 1, verticalShift);
 }
-
-function renderRow(app: Application, squareArea: number, amountOfRows: number) {
-  if (amountOfRows <= 0) return;
-
-  const amountOfRectRatio = app.screen.width / squareArea;
-  const amountOfRect = Math.ceil(amountOfRectRatio);
-
-  const verticalShift = (amountOfRows - 1) * squareArea;
-
-  renderCell(app, squareArea, amountOfRect, verticalShift);
-
-  renderRow(app, squareArea, amountOfRows - 1);
+function renderRow(app: Application, squareArea: number, index: number) {
+  const amountOfCells = Math.ceil(app.screen.width / squareArea);
+  const verticalShift = index * squareArea;
+  for (let i = 0; amountOfCells > i; i++) {
+    renderCell(app, squareArea, verticalShift, i);
+  }
 }
-
 export function renderGrid(app: Application, squareArea: number) {
   const amountOfRows = app.screen.height / squareArea;
-
-  renderRow(app, squareArea, amountOfRows);
+  for (let i = 0; amountOfRows > i; i++) {
+    renderRow(app, squareArea, i);
+  }
 }
