0495d71b05c0dbecbc6e52fcc2ef90c1520d5276
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-09-12 12:52:28 +0000

    feat: add next gen function to start

--- src/controls/start.ts
+++ src/controls/start.ts
@@ -1,7 +1,10 @@
+import { createNextGen } from "../engine/cell";
 import { game } from "../state/game";
 
 export function start() {
   game.status = "running";
 
   console.log("started");
+
+  createNextGen();
 }
