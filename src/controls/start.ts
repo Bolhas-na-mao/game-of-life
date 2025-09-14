722d53b4d6d364c376a34b2390eafbb2f6040d8b
Bruno Aseff <brunoaseff2@gmail.com>
2025-09-14 15:32:45 -0300

    feat: add click sounds

--- src/controls/start.ts
+++ src/controls/start.ts
@@ -1,10 +1,14 @@
+import { sound } from "@pixi/sound";
 import { createNextGen } from "../engine/cell";
 import { updateButton } from "../renderer/button";
 import { game } from "../state/game";
+import { renderGrid, resetGrid } from "../renderer/cell";
+import { sounds } from "../utils/sounds";
 
 function start() {
   if (game.status === "running") return;
 
+  sound.play(sounds.alias.click);
   game.status = "running";
   createNextGen();
 
@@ -14,7 +18,21 @@ function start() {
 function pause() {
   game.status = "paused";
 
+  sound.play(sounds.alias.click);
+
   updateButton(game.status);
 }
 
-export const controls = { start, pause };
+function restart() {
+  game.status = "idle";
+
+  sound.play(sounds.alias.click);
+
+  resetGrid();
+
+  renderGrid(14);
+
+  updateButton(game.status);
+}
+
+export const controls = { start, pause, restart };
