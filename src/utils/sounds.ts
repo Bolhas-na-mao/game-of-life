74bc612babe8953b3886dc32ed761b6657d65b72
Bruno Aseff <brunoaseff2@gmail.com>
2025-09-14 15:53:51 -0300

    refactor: improve audio utils

--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -1,15 +1,18 @@
 import { sound } from "@pixi/sound";
 
-const alias = {
+const alias = Object.freeze({
   hover: "hover_sound",
   click: "click_sound",
   background: "background_sound",
-};
+});
+
+let registered = false;
 
 function add() {
+  if (registered) return;
   sound.add(alias.hover, "assets/sounds/hover.mp3");
   sound.add(alias.click, "assets/sounds/click.mp3");
-  sound.add(alias.background, "assets/sounds/background.mp3");
+  registered = true;
 }
 
 export const sounds = { add, alias };
